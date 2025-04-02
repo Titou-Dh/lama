12586e5a44e2d4a7256d9d17a6a48f48c3065939
Titou-Dh <titoudh1@gmail.com>
2025-04-02 05:14:35 +0100

    Restore original database configuration and remove commented-out code

--- config/database.php
+++ config/database.php
@@ -1,28 +1,16 @@
 <?php
 
-// $db_server = 'localhost';
-// $db_user = 'root';
-// $db_password = '';
-// $db_name = 'lama_dev';
-
-// $cnx  = new PDO("mysql:host=$db_server;dbname=$db_name", $db_user, $db_password);
-
-
-$db_server = '10.123.0.78';
-$db_user = 'abddhe_admin_dev';
-$db_password = 'lama_dev@123'; // You need to add the password here
-$db_name = 'abddhe_admin_devv'; // Assuming the database name is the same
-$db_charset = 'utf8';
+$db_server = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'lama_dev';
 
 try {
     $cnx = new PDO(
-        "mysql:host=$db_server;dbname=$db_name;charset=$db_charset",
+        "mysql:host=$db_server;dbname=$db_name;charset=utf8",
         $db_user,
         $db_password,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
+
     );
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
