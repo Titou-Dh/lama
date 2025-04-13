739723c083809bedf8ad1815ad9efd0359c693c8
Titou-Dh <titoudh1@gmail.com>
2025-04-13 14:00:23 +0100

    Update sidebar navigation to dynamically highlight active page and replace SVG icons for improved consistency

---
+++ view/pages/dashboard/profile.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<?php
+include "../../../config/session.php";
+checkSession();
+
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+    <!-- CSS Files -->
+    <link
+        id="pagestyle"
+        href="../../styles/css/soft-ui-dashboard.css"
+        rel="stylesheet" />
+
+</head>
+
+<body class="g-sidenav-show bg-gray-100">
+    <?php include '../../partials/dashboar-sidebar.php' ?>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+        <?php include '../../partials/dashboard-navbar.php' ?>
+        profile
+
+    </main>
+
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../../scripts/core/popper.min.js"></script>
+    <script src="../../scripts/core/bootstrap.min.js"></script>
+    <script src="../../scripts/soft-ui-dashboard.js"></script>
+    <!-- <script src="../../scripts/soft-ui-dashboard.min.js"></script> -->
+</body>
+
+</html>
